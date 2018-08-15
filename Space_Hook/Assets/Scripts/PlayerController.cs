0cb7f62ed0fb57258a5724846b7772aa137d06ed
SEAN <cdnumber1@hotmail.com>
2018-08-15 22:18:27 +1000

    hook aims to mouse cursor while player is moving

--- Space_Hook/Assets/Scripts/PlayerController.cs
+++ Space_Hook/Assets/Scripts/PlayerController.cs
@@ -4,50 +4,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    private Vector3 hookDirection;
+    private Vector2 hookDirection;
+    public float distanceOfHook = 0.9f;
     private int rotationSpeed = 100;
     private float movementSpeed = 0.1f;
     public bool manualMovement = false;
-
+    public float angle;
     public GameObject hook;
-    public GameObject body;
+    private Vector3 mousePosition;
+    private Vector3 changeInPlayerPos;
+    private Vector3 initialPlayerPos;
 
-	// Use this for initialization
-	void Start () {
-        hook.transform.position = body.transform.position + new Vector3(0,0.9f,0);
+    // Use this for initialization
+    void Start () {
+        hook.transform.position = transform.position + new Vector3(0,0.9f,0);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if(Input.GetKey("left"))
-        {
-            hook.transform.RotateAround(body.transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
-        }
-        if(Input.GetKey("right"))
-        {
-            hook.transform.RotateAround(body.transform.position, Vector3.back, rotationSpeed * Time.deltaTime);
-        }
-
+        initialPlayerPos = transform.position;
+        
+        HookFaceMouse();
+        
         if (manualMovement)
         {
-            if (Input.GetKey("w"))
-            {
-                transform.position += Vector3.up * movementSpeed;
-            }
-            if (Input.GetKey("s"))
-            {
-                transform.position += Vector3.down * movementSpeed;
-            }
-            if (Input.GetKey("a"))
-            {
-                transform.position += Vector3.left * movementSpeed;
-            }
-            if (Input.GetKey("d"))
-            {
-                transform.position += Vector3.right * movementSpeed;
-            }
+            MovePlayer();
+            changeInPlayerPos = transform.position;
         }
+        HookTransform();
+    }
 
+    void MovePlayer()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += Vector3.up * movementSpeed;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += Vector3.down * movementSpeed;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * movementSpeed;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * movementSpeed;
+        }
+    }
+
+    void HookTransform()
+    {
+        hook.transform.position += (changeInPlayerPos - initialPlayerPos);
+    }
+
+    void HookFaceMouse()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        hookDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y).normalized;
+        Vector2 currentHookDirection = new Vector2(hook.transform.position.x - transform.position.x, hook.transform.position.y - transform.position.y);
+        angle = Vector2.SignedAngle(currentHookDirection, hookDirection);
+        
+        hook.transform.RotateAround(transform.position, transform.forward, angle);
     }
 }
