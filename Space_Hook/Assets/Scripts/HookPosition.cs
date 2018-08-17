d683a248519d38714858b6b58bd67bc7153a86c4
SEAN <cdnumber1@hotmail.com>
2018-08-17 18:49:06 +1000

    Hook Shoots

--- Space_Hook/Assets/Scripts/HookPosition.cs
+++ Space_Hook/Assets/Scripts/HookPosition.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HookPosition : MonoBehaviour
 {
-
+    public GameObject hookShot;
     public GameObject playerChar;
     private Vector2 hookDirection;
     private Vector3 mousePosition;
@@ -12,23 +12,24 @@ public class HookPosition : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
         transform.position = playerChar.transform.position + new Vector3(0, 0.9f, 0);
+        hookShot.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         hookDirection = new Vector2(mousePosition.x - playerChar.transform.position.x, mousePosition.y - playerChar.transform.position.y).normalized;
         Vector2 currentHookDirection = new Vector2(transform.position.x - playerChar.transform.position.x, transform.position.y - playerChar.transform.position.y);
         angle = Vector2.SignedAngle(currentHookDirection, hookDirection);
 
-       // transform.position = playerChar.transform.position + new Vector3(0, 0.9f, 0);
-
         transform.RotateAround(playerChar.transform.position, transform.forward, angle);
+
+        if(Input.GetMouseButton(0))
+        {
+            hookShot.SetActive(true);
+        }
     }
 }
---
+++ Space_Hook/Assets/Scripts/HookShoot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookShoot : MonoBehaviour {
+
+    public GameObject hookAim;
+    public GameObject attachment;
+    public float angle;
+    private Vector2 direction;
+    public GameObject body;
+    public Rigidbody2D hookrb;
+    public float thrust;
+    
+
+    // Use this for initialization
+    void Start () {
+
+        transform.position = hookAim.transform.position;
+        transform.rotation = hookAim.transform.rotation;
+        direction = new Vector2(transform.position.x - body.transform.position.x, transform.position.y - body.transform.position.y).normalized;
+        hookAim.SetActive(false);
+        hookrb.AddForce(direction * thrust);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(!hookrb)
+        {
+            transform.RotateAround(attachment.transform.position, transform.forward, -angle);
+        }
+      /*  if(hookrb.constraints == RigidbodyConstraints2D.FreezeAll)
+        {
+            transform.RotateAround(attachment.transform.position, transform.forward, angle);
+        }*/
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("attatch") == true)
+        {
+            attachment = col.gameObject;
+            Destroy(hookrb);//destroys rigibody
+
+            //hookrb.constraints = RigidbodyConstraints2D.FreezeAll; //stops all movement
+        }
+    }
+}
