fe39fbbde6806d44e8414a8366fd026f7e9571bf
unknown <101182527@AS409-012.ds.swin.edu.au>
2018-09-07 17:58:26 +1000

    Tried random generation level design

--- Space_Hook/Assets/Scripts/AsteroidBehavior.cs
+++ Space_Hook/Assets/Scripts/AsteroidBehavior.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidBehavior : MonoBehaviour {
+public class AsteroidBehavior : MonoBehaviour
+{
 
     public float speed = 30f;
     public bool clockwise = true;
@@ -13,21 +14,33 @@ public class AsteroidBehavior : MonoBehaviour {
     public GameObject player;
     public float maxDistance;
     public bool imAttatched;
-    
-    private PlayerController playerC;
-    
 
-	// Use this for initialization
-	void Start () {
-        forcfield.SetActive(false);
+    private PlayerController playerC;
+
+    public float minScale, maxScale;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Sets scale of object 
+        SetTransform();
+
+        transform.parent = GameObject.FindGameObjectWithTag("Asteroids").transform;
+        forcfield = GameObject.FindGameObjectWithTag("Forcefield");
+        hookAttatch = GameObject.FindGameObjectWithTag("HookAttach");
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // forcfield.SetActive(false);
         imAttatched = false;
         playerC = player.GetComponent<PlayerController>();
         maxDistance = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(imAttatched == false)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (imAttatched == false)
         {
             maxDistance = 0;
         }
@@ -44,17 +57,17 @@ public class AsteroidBehavior : MonoBehaviour {
             }
         }
 
-        if(imAttatched)
+        if (imAttatched)
         {
-            if(playerC.state != "attatch")
+            if (playerC.state != "attatch")
             {
                 playerC.state = "attatch";
                 playerC.attatchedTo = attatchedTo;
-                if(maxDistance == 0)
+                if (maxDistance == 0)
                 {
                     maxDistance = (playerC.transform.position - forcfield.transform.position).magnitude;
                 }
-                
+
             }
             if (maxDistance < 2.3)
             {
@@ -68,6 +81,24 @@ public class AsteroidBehavior : MonoBehaviour {
             float angle = Vector2.SignedAngle(currentHookDirection, hookDirection);
 
             hookAttatch.transform.RotateAround(forcfield.transform.position, transform.forward, angle);
+        }
+
+    }
+
+    public void SetTransform()
+    {
+        float objectScale = Random.Range(minScale, maxScale);
+        transform.localScale = new Vector3(objectScale, objectScale, 0);
+
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360)));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "attatch")
+        {
+            float spawnRnge = GetComponentInParent<SpawnLevel>().spawnRange;
+            transform.position = new Vector3(Random.Range(-spawnRnge, spawnRnge), Random.Range(-spawnRnge, spawnRnge), 0);
         }
     }
 }
---
+++ Space_Hook/Assets/Scripts/SpawnLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLevel : MonoBehaviour {
+
+    public GameObject levelObject;
+
+    public float spawnRange;
+
+    public int amtToSpawn;
+
+	void Start () {
+
+        for (int i = 0; i < amtToSpawn; i++)
+        {
+           var newAsteroid = Instantiate(levelObject, new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0), Quaternion.identity);
+        }		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
