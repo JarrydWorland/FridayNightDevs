95d95a2fefc33a445c4e8619ce915023ec5f6be9
Jacques Van Niekerk <jshark46@gmail.com>
2018-11-02 13:57:03 +1100

    Add Color Change to BlackHoles

--- Space_Hook/Assets/Scripts/WormLink.cs
+++ Space_Hook/Assets/Scripts/WormLink.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,17 +7,30 @@ using UnityEngine;
 
 public class WormLink : MonoBehaviour
 {
+    public Color OrbColour = new Color(189, 2, 255);
+    public Color RimColour = new Color(135,0, 255);
+    public Gradient WaveGradient;
     public float WarpCoolDown = 2;
     private WormHole hole1;
     private WormHole hole2;
     private Dictionary<GameObject,float> WrapedObjects;
-    // Use this for initialization
+    // Use this for initializ135ation
     void Awake ()
     {
         WrapedObjects = new Dictionary<GameObject, float>();
         var holes = GetComponentsInChildren<WormHole>();
         hole1 = holes[0];
         hole2 = holes[1];
+        for (int i = 0; i < holes.Length; i++)
+        {
+             var particles = holes[i].GetComponentsInChildren<ParticleSystem>();
+            particles[1].startColor = RimColour;
+            var COL1 = particles[2].colorOverLifetime;
+            COL1.color = WaveGradient;
+            var COL2 = particles[3].colorOverLifetime;
+            COL2.color = WaveGradient;
+            particles[4].startColor = OrbColour;
+        }
     }
 
     public void Warp(GameObject WarpedTarget, WormHole enterHole)
@@ -41,6 +55,7 @@ public class WormLink : MonoBehaviour
             {
                 LevelManger.Instance.player.Detatch();
             }
+            // TODO fix Camera to zoom to then  telaport;
             CompleteCameraController.Instance.MoveCameraTo(WarpedTarget.transform.position);
         }
         else if (WarpedTarget.GetComponent<AsteroidBehavior>())
